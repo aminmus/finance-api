4267c143e47392513206a26091bf506fa17b80a6
Amin Mushehid <amin.mushehid@gmail.com>
2020-08-25 23:29:55 +0200

    Add Portfolio Nexus type and CRUD operations

---
+++ api/graphql/Portfolio.ts
@@ -0,0 +1,31 @@
+import { schema } from "nexus";
+
+schema.objectType({
+  name: 'Portfolio',
+  definition(t) {
+    t.model.id();
+    t.model.updatedAt();
+    t.model.createdAt();
+    t.model.name();
+    t.model.description();
+    t.model.assets();
+    t.model.User();
+  },
+});
+
+schema.extendType({
+  type: 'Query',
+  definition(t) {
+    t.crud.portfolio();
+    t.crud.portfolios();
+  },
+});
+
+schema.extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.crud.createOnePortfolio();
+    t.crud.deleteOnePortfolio();
+    t.crud.updateOneUser();
+  },
+});
