8d6c4d249431ee3c4099772fe3269613077248d1
Amin Mushehid <amin.mushehid@gmail.com>
2020-08-29 16:28:43 +0200

    Add CustomAsset and HistoricalValue GraphQL types

---
+++ api/graphql/HistoricalValue.ts
@@ -0,0 +1,22 @@
+import { schema } from "nexus"
+
+schema.objectType({
+  name: 'HistoricalValue',
+  definition(t) {
+    t.model.id();
+    t.model.createdAt();
+    t.model.updatedAt();
+    t.model.date();
+    t.model.asset();
+    t.model.unitPrice();
+  },
+});
+
+schema.extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.crud.createOneHistoricalValue();
+    t.crud.updateOneHistoricalValue();
+    t.crud.deleteOneHistoricalValue();
+  },
+});
