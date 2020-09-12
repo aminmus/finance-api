a5be351be4134561afc4639e25821538c100dca7
Amin Mushehid <amin.mushehid@gmail.com>
2020-09-12 15:23:32 +0200

    Fix resolution of baseAsset for PublicAsset
    Fixes #10.

--- api/graphql/PublicAsset.ts
+++ api/graphql/PublicAsset.ts
@@ -1,14 +1,26 @@
-import { schema } from "nexus"
+import { schema } from 'nexus';
 
 schema.objectType({
   name: 'PublicAsset',
-  description: 'Publicly traded asset. For assets that are traded in a known and supported public market.',
+  description:
+    'Publicly traded asset. For assets that are traded in a known and supported public market.',
   definition(t) {
     t.model.id();
     t.model.symbol();
     t.model.market();
-    t.model.baseAsset();
-  }
+    t.model.baseAsset({
+      async resolve(root, args, ctx, info, originalResolve) {
+        const baseAsset = await originalResolve(root, args, ctx, info);
+        if (baseAsset) {
+          // if baseAsset exists in db (like during a "get" query)
+          return baseAsset;
+        } else {
+          // get baseAsset from the mutation resolver instead, (works regardless of TS error)
+          return root.baseAsset;
+        }
+      },
+    });
+  },
 });
 
 schema.extendType({
@@ -23,7 +35,37 @@ schema.extendType({
   type: 'Mutation',
   definition(t) {
     t.crud.createOnePublicAsset();
-    t.crud.deleteOnePublicAsset();
+    t.crud.deleteOnePublicAsset({
+      async resolve(_root, args, ctx, _info) {
+        if (args.where.id) {
+          // 1. delete public asset
+          const deletePublicAsset = ctx.db.publicAsset.delete({
+            where: { id: args.where.id },
+            include: {
+              baseAsset: { include: { portfolio: true, transactions: true } },
+            },
+          });
+          // 2. delete asset transactions
+          const deleteAssetTransactions = ctx.db.transactionRecord.deleteMany({
+            where: { assetId: args.where.id },
+          });
+          // 3. delete asset
+          const deletedBaseAsset = ctx.db.asset.delete({
+            where: { id: args.where.id },
+          });
+
+          const dbTransactionResponse = await ctx.db.$transaction([
+            deletePublicAsset,
+            deleteAssetTransactions,
+            deletedBaseAsset,
+          ]);
+
+          return dbTransactionResponse[0];
+        } else {
+          throw new Error('An error occurred, no id was given.');
+        }
+      },
+    });
     t.crud.updateOnePublicAsset();
   },
 });
