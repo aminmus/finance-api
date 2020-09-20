6050f00b5040ce5f949cb9994deef66936c2aefc
Amin Mushehid <amin.mushehid@gmail.com>
2020-09-20 22:39:09 +0200

    Install graphql-shield via nexus-plugin-shield

--- api/app.ts
+++ api/app.ts
@@ -2,6 +2,10 @@ import { use } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
 import dotenv from 'dotenv';
 
+import permissions from './authorization/permissions';
+
 dotenv.config()
 
+// Nexus plugins
 use(prisma({ features: { crud: true } }));
+use(permissions);
