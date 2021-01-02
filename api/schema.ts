164466faad9938a416dd63b553107301e054623d
Amin Mushehid <amin.mushehid@gmail.com>
2021-01-02 20:17:29 +0100

    Use correct nexus package and use permissions

--- api/schema.ts
+++ api/schema.ts
@@ -1,39 +1,40 @@
-import { makeSchema } from '@nexus/schema';
+import { declarativeWrappingPlugin, makeSchema } from 'nexus';
 import { join } from 'path';
-import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema';
+import { nexusPrisma } from 'nexus-plugin-prisma';
 import { DateTimeResolver } from 'graphql-scalars';
 import { applyMiddleware } from 'graphql-middleware';
 
 import * as types from './graphql';
 import permissions from './authorization/permissions';
 
-export const schema = applyMiddleware(makeSchema({
-
-  types,
-  plugins: [
-    nexusSchemaPrisma({
-    experimentalCRUD: true,
-    scalars: {
-      DateTime: DateTimeResolver,
-    },
-  }),
-],
-  outputs: {
-    typegen: join(process.cwd(), '..', 'nexus-typegen.ts'),
-    schema: join(process.cwd(), '..', 'schema.graphql'),
-  },
-  typegenAutoConfig: {
-    contextType: 'ContextModule.Context',
-    sources: [{
-      source: join(__dirname, './types/context.ts'),
-      alias: 'ContextModule',
-    },
-    {
-
-      source: '@prisma/client',
-      alias: "prisma",
-    }
-  ],
-  },
-// }), permissions);
-}));
+export const schema =
+  applyMiddleware(
+    makeSchema({
+      types,
+      plugins: [
+        declarativeWrappingPlugin(),
+        nexusPrisma({
+          experimentalCRUD: true,
+          scalars: {
+            DateTime: DateTimeResolver,
+          },
+        }),
+      ],
+      outputs: {
+        typegen: join(process.cwd(), '..', 'nexus-typegen.ts'),
+        schema: join(process.cwd(), '..', 'schema.graphql'),
+      },
+      contextType: {
+        export: 'Context',
+        module: join(__dirname, './types/context.ts'),
+        alias: 'ContextModule',
+      },
+      sourceTypes: {
+        modules: [{
+          module: '@prisma/client',
+          alias: "prisma",
+        }]
+      }
+    }),
+    permissions
+  );
