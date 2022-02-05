3ec644c87a6875c702f27e047604905beb3d3b11
Amin Mushehid <amin.mushehid@gmail.com>
2022-02-05 23:12:44 +0100

    Only check isPortfolioOwner using root

--- api/authorization/rules.ts
+++ api/authorization/rules.ts
@@ -3,16 +3,13 @@ import { Context } from '../types/context';
 
 import getUserId from '../utils/getUserId';
 
-export const isAuthenticated = rule({ cache: 'contextual' })((_root, _args, ctx, _info) => Boolean(getUserId(ctx)));
+export const isAuthenticated = rule({ cache: 'contextual' })((_root, _args, ctx: Context, _info) => Boolean(getUserId(ctx)));
 
-export const isPortfolioOwner = rule({ cache: 'contextual' })(async (_root, args, ctx: Context, _info) => {
-  const portfolioId = args.where.id;
-  const userId = getUserId(ctx);
+export const isPortfolioOwner = rule({ cache: 'contextual' })((root, args, ctx: Context, _info) => ctx.req.user.id === root.owner.id);
 
-  const realOwner = await ctx.prisma.portfolio.findUnique({ where: { id: portfolioId } }).owner();
+// const realOwner = await ctx.prisma.portfolio.findUnique({ where: { id: portfolioId } }).owner();
 
-  return realOwner?.id === userId;
-});
+// return realOwner?.id === userId;
 
 // export const isAssetOwner = rule({ cache: 'contextual' })(async (_root, args, ctx: Context, _info) => {
 //   const assetId = args.where.id;
