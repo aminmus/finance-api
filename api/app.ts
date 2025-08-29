3225d9c4bb6bccf77966120d7f5696d07a7615fc
Amin Mushehid <amin.mushehid@gmail.com>
2025-08-29 22:57:08 +0200

    Pass in the prefilled playground data to app

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 // import { PrismaClient } from '@prisma/client';
 import prismaClient from './prismaClient';
 
@@ -13,16 +15,31 @@ const app = express();
 
 app.use(authenticateUser);
 
+const playgroundQuery = fs.readFileSync(
+  path.join(__dirname, '..', 'playground', 'requests.graphql'),
+  'utf8',
+);
+
 const apollo = new ApolloServer({
   schema,
   context: ({ req }) => ({
     prisma: prismaClient,
     req,
   }),
+  playground: {
+    tabs: [
+      {
+        endpoint: '/graphql',
+        query: playgroundQuery,
+      },
+    ],
+  },
 });
 
 apollo.applyMiddleware({ app });
 
 app.listen(process.env.API_PORT, () => {
-  console.log(`🚀 GraphQL service ready at http://localhost:${process.env.API_PORT}/graphql`);
+  console.log(
+    `🚀 GraphQL service ready at http://localhost:${process.env.API_PORT}/graphql`,
+  );
 });
